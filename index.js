2fd7f70fc89cdb9a7f00e94c51764d32af7cabf5
setraniainabruno <brunoharison18@yahoo.com>
2025-07-25 13:06:25 +0300

    Premiere commit

---
+++ index.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Configuration Nodemailer
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  }
+});
+
+
+app.post('/api/send-email', async (req, res) => {
+  const { to, subject, html } = req.body;
+
+  if (!to || !subject || !html) {
+    return res.status(400).json({ error: 'Tous les champs sont requis' });
+  }
+  try {
+    const info = await transporter.sendMail({
+      from: `"Setraniaina Bruno" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html
+    });
+    res.status(200).json({ success: true, message: 'Email envoyé avec succès' });
+  } catch (error) {
+    res.status(500).json({ error: "Échec de l'envoi de l'email" });
+  }
+});
+
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+  console.log(`Serveur en cours d'exécution sur le port : ${PORT}`);
+});
